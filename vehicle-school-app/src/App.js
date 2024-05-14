fd4ed44c02e1c41145c1604fff042c34069f3e5a
ivica1 <imedvi00@fesb.hr>
2024-05-14 18:23:44 +0200

    Add routing for vehicle model

--- vehicle-school-app/src/App.js
+++ vehicle-school-app/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import VehicleMakePage from "./pages/VehicleMakePage";
+import VehicleModelPage from "./pages/VehileModelPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' Component={VehicleMakePage} />
+        <Route path='/vehicle-models' Component={VehicleModelPage} />
       </Routes>
     </Router>
   );
